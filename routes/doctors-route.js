4313eb9f5b9561fe158739906b96b09a10adcf56
tluwaniMS <tluwani2020@gmail.com>
2022-03-10 14:20:53 +0200

    adding-required-routes-for-pagination

--- routes/doctors-route.js
+++ routes/doctors-route.js
@@ -1,4 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const { DoctorModel } = require("../database-models/doctor");
+
+router.get("/get-initialising-page/:dataLimit", async (req, res) => {
+  const { dataLimit } = req.params;
+
+  const allDoctors = await DoctorModel.find({});
+  const doctors = await DoctorModel.find({}).limit(dataLimit);
+
+  const totalDoctorsCount = allDoctors.length;
+  const totalPagesToBeRendered = Math.ceil(totalDoctorsCount / dataLimit);
+
+  res.status(200).send({ data: doctors, totalPagesToBeRendered: totalPagesToBeRendered });
+});
+
+router.get("/get-next-page/:dataLimit/:skipAmount", async (req, res) => {
+  const { dataLimit, skipAmount } = req.params;
+
+  const doctors = await DoctorModel.find({}).skip(skipAmount).limit(dataLimit);
+
+  res.status(200).send({ data: doctors });
+});
 
 module.exports = router;
